3d9298f2fbba9eef6a020c854094e2436c4d3076
Harshil Thummar <harshil@happypet.care>
2026-02-10 07:07:49 +0530

    Add new site URL to monitored list in monitor.js for enhanced monitoring capabilities.

--- monitor.js
+++ monitor.js
@@ -9,6 +9,7 @@ const SITES = [
   "https://app.happypet.tech",
   "https://app.happypet.care",
   "https://www.happypet.tech",
+  "https://ppa-api.happypet.care"
 ];
 
 const STATE_FILE = "site_state.json";
